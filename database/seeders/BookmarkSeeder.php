be33e9183fd3d410b70501438beef0aa0f5c99a7
livia789 <t-livia.livia@traveloka.com>
2024-06-17 14:25:04 +0700

    add bookmark in card

--- database/seeders/BookmarkSeeder.php
+++ database/seeders/BookmarkSeeder.php
@@ -23,6 +23,106 @@ class BookmarkSeeder extends Seeder
             [
                 "user_id" => "3",
                 "recipe_id" => "3",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "4",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "5",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "6",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "7",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "8",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "9",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "10",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "11",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "12",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "13",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "14",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "15",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "16",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "17",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "18",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "19",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "20",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "21",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "22",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "23",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "24",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "25",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "26",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "27",
+            ],
+            [
+                "user_id" => "3",
+                "recipe_id" => "28",
             ]
         ]);
     }
--- resources/views/templates/recipeCard2.blade.php
+++ resources/views/templates/recipeCard2.blade.php
@@ -1,3 +1,9 @@
+<?php
+    use Illuminate\Support\Str;
+    $isBookmarked = Auth::user() && Auth::user()->bookmarks()->where('recipe_id', $recipe->id)->exists();
+    $bookmarkImage = $isBookmarked ? 'bookmark_black.png' : 'bookmark_white.png';
+?>
+
 <a href="/recipeDetail/{{ $recipe->id }}" style="text-decoration: none; color:black;">
     @if($recipe->pivot && $recipe->pivot->created_at !== null)
         <p style="margin-bottom:0px">
@@ -5,31 +11,46 @@
         </p>
     @endif
     <div class="recipeCardContainer">
-        <div class="recipeCardImg" style="position: relative;">
+        <div class="recipeCardImg" style="position: relative; margin-bottom:10px">
             <img src="{{ asset($recipe->img) }}" class="card-img-top" alt="...">
             @if ($recipe->company_id !== null)
                 <img src="{{ asset($recipe->company()->first()->img_logo) }}" class="card-img-top companyLogo" alt="company logo">
             @endif
         </div>
-        <b>{{ $recipe->name }}</b>
+        <div class="d-flex">
+            <b style="flex:1; overflow-wrap:break-word;max-width:90%; width:90%">{{ Str::limit($recipe->name, 35) }}</b>
+            <img src="/assets/icons/{{$bookmarkImage}}"  class="picon bookmarkIcon" style="margin:0px;" id="recipeCardBookmarkButton" isBookmarked="{{$isBookmarked}}" onclick="recipeCard_toggleBookmark(event, this)" onmouseover="mousein_blackBookmarkIcon(this)" onmouseout="mouseout_whiteBookmarkIcon(this)" recipe_id="{{$recipe->id}}" alt="bookmark_icon">
+        </div>
         @include('templates/rating', ['rating_avg' => $recipe->reviews->avg('rating')])
 
         <div style="display:flex; margin-bottom:10px">
             <img src="/assets/icons/empty_heart.png" class="picon" style="margin-right:5px;" alt="heart_icon">
-            {{ $recipe->reviews->count('rating') }} ulasan
+            <p>
+                {{ $recipe->reviews->count('rating') }} ulasan
+            </p>
         </div>
         <div style="display:flex;">
-            <img src="/assets/icons/time_icon.png" class="picon" alt="time_icon">
-            {{ $recipe->getDurationStr() }}
+            <img src="/assets/icons/time_icon.png" class="picon" style="margin-right:5px;" alt="time_icon">
+            <p>
+                {{ $recipe->getDurationStr() }}
+            </p>
         </div>
+        @if ($recipe->type == 'private')
+            <h6 style="color: red; width: 80px; text-align: center; border: 1px red solid; border-radius: 16px; padding: 2px 5px; float: right; ">
+                Privat
+            </h6>
+        @elseif (Auth::check() && $recipe->user_id == Auth::user()->id)
+            <h6 style="color: green; width: 80px; text-align: center; border: 1px green solid; border-radius: 16px; padding: 2px 5px; float: right; ">
+                Publik
+            </h6>
+        @endif
     </div>
-
 </a>
 
 <style>
     .recipeCardContainer {
         width: 250px;
-        height: 350px;
+        height: 370px;
         background-color: white;
         border-radius: 7px;
         border: 3px solid black;
@@ -61,4 +82,95 @@
         height: auto;
         z-index: 1;
     }
+
+    .recipeCardContainer p{
+        margin: 0px;
+    }
+
+    @media (max-width: 600px) {
+        .recipeCardContainer {
+            width: 40vw;
+            height: 60vw;
+            background-color: white;
+            border-radius: 7px;
+            border: 3px solid black;
+            padding: 15px;
+            margin:0px 20px 10px 0px;
+        }
+
+        p, b{
+            font-size: 12px;
+        }
+
+        .recipeCardContainer .picon {
+            width: 15px;
+            height: 15px;
+            margin: auto 0px;
+        }
+
+        .recipeCardContainer .starIcon {
+            width: 12px;
+            height: 12px;
+        }
+
+        .recipeCardContainer .bookmarkIcon{
+            width: 20px;
+            height: 20px;
+        }
+    }
+
+
 </style>
+
+<script>
+    var user_id = '{{Auth::user() ? Auth::user()->id : -1}}';
+    var token = '{{csrf_token()}}';
+    
+    function mousein_blackBookmarkIcon(data){
+        $(document).ready(function () {
+            data.src = '/assets/icons/bookmark_black.png';
+        });
+    }
+
+    function mouseout_whiteBookmarkIcon(data){
+        $(document).ready(function () {
+            console.log("on mouse quit, isBookmarked : " + data.getAttribute('isBookmarked'));
+            let isBookmarked = data.getAttribute('isBookmarked');
+            if(isBookmarked === true || isBookmarked === 'true' || isBookmarked === "1"){
+                // alert("masuk condition isBookmarked")
+                data.src = '/assets/icons/bookmark_black.png';
+            }else{
+                data.src = '/assets/icons/bookmark_white.png';
+            }
+        });
+    }
+
+    function recipeCard_toggleBookmark(event, data){
+        event.preventDefault();
+        let recipe_id = data.getAttribute('recipe_id');
+
+        if(user_id == -1){
+            alert("Mohon login untuk dapat menyimpan (markah) resep");
+            return;
+        }
+
+        $.ajax({
+            url: '/toggleBookmark',
+            type: 'POST',
+            data: {
+                user_id: user_id,
+                recipe_id: recipe_id,
+                _token: token
+            },
+            success: function (response) {
+                let isBookmarked = response.isBookmarked;
+                data.setAttribute('isBookmarked', isBookmarked);
+                data.src = isBookmarked ? '/assets/icons/bookmark_black.png' : '/assets/icons/bookmark_white.png';
+            },
+            error: function (e) {
+                msg = e.status == 401 ? "[Error] Mohon login untuk dapat menyimpan resep" : "Gagal memproses permintaan simpan resep"
+                alert(msg);
+            }
+        });
+    }
+</script>
