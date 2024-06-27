bf9de1c72aa4851bbf04df295416f5d24cd78a6b
salmon <125944630+Livia789@users.noreply.github.com>
2024-06-27 23:25:40 +0700

    disable user from reviewing their own recipes (#48)
    * add text if there's no cooking history
    
    * disable user from reviewing their own recipes
    
    * add scroller for recipe review section

--- resources/views/myCookingHistory.blade.php
+++ resources/views/myCookingHistory.blade.php
@@ -6,6 +6,19 @@
     <div class="">
         <h1>Riwayat Memasak</h1>
 
+        @if($recipes->isEmpty())
+            <div>
+                <div style="text-align:center">
+                    <i>Saat ini belum ada riwayat memasak</i>
+                </div>
+                <div class="sharpBox btnSearchRecipe">
+                    <a href="/search" style="text-decoration: none; color:black">
+                        Temukan Resep
+                    </a>
+                </div>
+            </div>
+        @endif
+
         <div class="d-flex flex-wrap" style="width:100%;">
             @foreach($recipes as $recipe)
                 @include('templates/recipeCard2', compact('recipe'))
